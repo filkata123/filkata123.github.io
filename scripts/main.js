7a8d09b4416c20b1d65f9e94f857975942d679b2
filkata123 <57031998+filkata123@users.noreply.github.com>
2020-01-22 01:14:46 +0100

    Mock website

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/diplo.png'){
+        myImage.setAttribute('src', 'images/diplo2.jpg');
+    }else{
+        myImage.setAttribute('src', 'images/diplo.png');
+    }           
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName || myName === null) {setUserName();}
+    else{
+        localStorage.setItem('name', myName);
+        myHeading.textContent = 'Diplodocus for, ' + myName;
+    }
+}
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Diplodocus for ' + storedName;
+}
+
+myButton.onclick = function() {
+    setUserName();
+  }
